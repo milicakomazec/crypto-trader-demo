6d7df28516f6c89b78b72a4b78d4d15ed5b3c3ff
milicakomazec <milicakomazec.it@gmail.com>
2022-08-27 11:23:16 +0200

    add Table component

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,9 @@
+import "./Table.scss"
+
+import React from "react"
+
+const Table = () => {
+  return <div className="table-container">---space for table---</div>
+}
+
+export default Table
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import "./index.scss"
+import "./normalize.scss"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
