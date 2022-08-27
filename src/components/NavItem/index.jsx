ef894db8838541a2f64d3085a8a8ba8b6c9a7cdc
milicakomazec <milicakomazec.it@gmail.com>
2022-08-27 14:41:41 +0200

    add react-responsive, update design

--- src/components/NavItem/index.jsx
+++ src/components/NavItem/index.jsx
@@ -1,12 +1,16 @@
 import "./NavItem.scss"
 
 import React from "react"
+import { useMediaQuery } from "react-responsive"
 
 const NavItem = ({ title, img }) => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 480px)",
+  })
   return (
     <div className="nav-item">
       <img src={`../../media/${img}`} alt="" className="nav-icon" />
-      {title}
+      {!isMobile && <div className="nav-title">{title}</div>}
     </div>
   )
 }
