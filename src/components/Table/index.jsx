311ff73c173c4b7cbfb56f227a382e9ce10487bd
milicakomazec <milicakomazec.it@gmail.com>
2022-08-29 01:03:59 +0200

    update table component

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,44 +1,24 @@
 import "./Table.scss"
 
 import React from "react"
-import { useMediaQuery } from "react-responsive"
 
-const Table = ({ tblHeaders, tblData, tblOnClick }) => {
-  const isMobile = useMediaQuery({
-    query: "(max-width: 480px)",
-  })
+const Table = ({ tblHeaders, tblData }) => {
   return (
     <div className="tbl-container">
-      {!isMobile && (
-        <table className="tbl-wide">
-          <thead>
-            {tblHeaders.map(tblHeader => (
-              <th>{tblHeader}</th>
+      <table className="tbl">
+        <thead>
+          <tr>
+            {tblHeaders.map((tblHeader, index) => (
+              <th key={index}>{tblHeader}</th>
             ))}
-          </thead>
-          <tbody>
-            {tblData.map(row => (
-              <tr>
-                <td>
-                  <span className="symbol" role="button" onClick={tblOnClick}>
-                    {row.name}
-                  </span>
-                </td>
-                <td>{row.desc}</td>
-                <td>{row.id}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      {isMobile &&
-        tblData.map(el => (
-          <table className="tbl-narrow">
-            <tr onClick={tblOnClick}>{el.name}</tr>
-            <tr>{el.id}</tr>
-            <tr>{el.desc}</tr>
-          </table>
-        ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tblData.map((row, index) => {
+            return <tr key={index}>{row.rowItems.map(item => item)}</tr>
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
