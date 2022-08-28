f7a7f5352bde7a28120c988249fb52a2a08eb7dc
milicakomazec <milicakomazec.it@gmail.com>
2022-08-28 22:23:13 +0200

    add Loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,13 @@
+import "./Loading.scss"
+
+import React from "react"
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="lds-dual-ring" />
+    </div>
+  )
+}
+
+export default Loading
